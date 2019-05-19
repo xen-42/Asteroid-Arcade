29d474dcfd6c20c0d4bf00413178366c534988a9
NicholasConnors <nicholasconnors@hotmail.ca>
2019-05-18 21:31:11 -0400

    Update AndroidLauncher.java
    Typo

--- AsteroidArcade/android/src/njc/ufo/AndroidLauncher.java
+++ AsteroidArcade/android/src/njc/ufo/AndroidLauncher.java
@@ -12,7 +12,7 @@ public class AndroidLauncher extends AndroidApplication {
 		super.onCreate(savedInstanceState);
 		
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		cfg.useWakelock = true;
+		config.useWakelock = true;
 		
 		initialize(new Game(), config);
 	}
